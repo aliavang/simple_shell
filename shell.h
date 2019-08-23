e9f27f9b7083a406f98d791f7c8edd751be14866
aliavang <714@holbertonschool.com>
2019-08-23 20:03:03 +0000

    First commit, header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELLBACCA_H
+#define SHELLBACCA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int _strcmp(char *s1, char *s2);
+
+#endif /*SHELLBACCA_H*/
