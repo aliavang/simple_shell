efacb8c41325ba1b9830456ecc3437d4dd7f3b84
aliavang <714@holbertonschool.com>
2019-08-26 22:02:22 +0000

    Added forking as well as function to break string

--- prompter.c
+++ prompter.c
@@ -1,20 +1,33 @@
-#include "shell.h"
+#include "shellbacca.h"
 
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
 	char *buff = NULL;
-        size_t buff_size = 0;
-        ssize_t chara;
+	size_t buff_size = 0;
+	ssize_t chara;
+	char **args;
+	pid_t childPid;
+	int status;
 
-        while (1)
-        {
-		write(STDOUT_FILENO, "MAY THE FORCE BE WITH YOU$ ", 28);
-                chara = getline(&buff, &buff_size, stdin);
-                if (chara == -1)
-                {
-                        free(buff);
-                        return (EXIT_FAILURE);
-                }
+	while (1)
+	{
+		write(STDOUT_FILENO, "MAY THE FORCE BE WITH YOU$ ", 27);
+		chara = getline(&buff, &buff_size, stdin);
+		if (chara == -1)
+		{
+			free(buff);
+			return (EXIT_SUCCESS);
+		}
+		args = strtokenizer(buff, " \n");
+		childPid = fork();
+		if (childPid == 0)
+		{
+			execve(args[0], args, NULL);
+		}
+		else
+		{
+			wait(&status);
+		}
 	}
 	return (EXIT_SUCCESS);
 }
