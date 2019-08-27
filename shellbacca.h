08d7c34963c971a7abcf97168e77c8d8146a61ab
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-27 00:05:35 +0000

    added 3 new prototype name

--- shellbacca.h
+++ shellbacca.h
@@ -12,6 +12,10 @@
 int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
 int tokcount(char *s, const char *delim);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
 char **strtokenizer(char *s, const char *delim);
 
 #endif /*SHELLBACCA_H*/
