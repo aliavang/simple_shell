0e8bad867f5d1a59941492a276a3da1f6669b7b8
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-27 21:21:05 +0000

    change env to environ

---
+++ _env.c
@@ -0,0 +1,18 @@
+#include "shellbacca.h"
+
+/**
+ * _env - entry point
+ * Description: print the environment
+ *
+ */
+void _env(void)
+{
+	int i;
+	char **env = environ;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		write(STDOUT_FILENO, env[i], strlen(env[i]));
+		_putchar('\n');
+	}
+}
