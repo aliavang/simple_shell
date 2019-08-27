cd1d466d16b2f1520493385c5e1cc420e33dadc8
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-27 00:02:59 +0000

    added 3 new helper fx

--- string_funcs.c
+++ string_funcs.c
@@ -22,7 +22,6 @@ int _strcmp(char *s1, char *s2)
 	return (0);
 }
 
-#include "shellbacca.h"
 /**
  * _strdup - Duplicate a string to newly allocated space
  * @str: String to be duplicated
@@ -55,4 +54,61 @@ char *_strdup(char *str)
 	}
 	a[i] = '\0';
 	return (a);
+}
+/**
+ * _strlen - entry point
+ * @s: parameter for strings
+ *
+ * Description: returns the length of a string
+ * Return: a value
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * _strncmp - Comapres two strings up to a certain character
+ * @s1: String one
+ * @s2: String two
+ * @n: number of characters need to be compared
+ *
+ * Return: the number of the characters counted
+ */
+int _strncmp(char *s1, char *s2, int n)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (s1[i] == s2[i])
+			counter += 1;
+	}
+	return (counter);
+}
+/**
+ * _strcat - Glue two strings together
+ * @dest: String one
+ * @src: String two
+ *
+ * Return: Pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
