b287977afd22173fbaab2be5195dd164df0e826f
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-27 21:34:16 +0000

    switch strdup with strcpy

--- string_funcs.c
+++ string_funcs.c
@@ -23,37 +23,23 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * _strdup - Duplicate a string to newly allocated space
- * @str: String to be duplicated
+ * _strcpy - entry point
+ * @dest: pointer to an arryay
+ * @src: pointer to an array
  *
- * Return: Pointer to newly allocated space
+ * Description: copy a string
+ * Return: dest
  */
-char *_strdup(char *str)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
-	int j;
-	int strlen = 0;
-	char *a = NULL;
+        int i;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (j = 0; str[j] != '\0'; j++)
-	{
-		strlen++;
-	}
-	a = malloc(sizeof(char) * (strlen + 1));
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		a[i] = str[i];
-	}
-	a[i] = '\0';
-	return (a);
+        for (i = 0; src[i] != '\0'; i++)
+        {
+                dest[i] = src[i];
+        }
+        dest[i] = '\0';
+        return (dest);
 }
 /**
  * _strlen - entry point
