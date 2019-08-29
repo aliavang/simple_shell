537b32da4ae9560df46a860ab4e0c33bd0a81a4c
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-08-29 00:05:40 +0000

    fix Betty error by added description

--- _exit.c
+++ _exit.c
@@ -1,11 +1,17 @@
 #include "shellbacca.h"
-
+/**
+ * _bye - main entry, exit the program
+ * @buff: pointer to the arguments
+ * @exit_status: a variable integer
+ *
+ * Return: 0 on success or 1 on failure
+ */
 
 void _bye(char *buff, int exit_status)
 {
 	int i = exit_status;
 
 	if (_strcmp(buff, "exit\n") == 0)
-		exit (i);
+		exit(i);
 }
 
